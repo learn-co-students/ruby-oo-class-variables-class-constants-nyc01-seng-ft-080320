bbafed86d6e946fda284f7994bddefe66cb67130
kain <kaindoneson@gmail.com>
2020-08-04 16:49:20 -0400

    I think u cant use uniq on constants

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,15 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_accessor :color, :size, :material, :condition;
+  attr_reader :brand;
 
+  BRANDS = [];
+  
   def initialize(brand)
-    @brand = brand
+    @brand = brand;
+    # I think we cant use uniq cause BRANDS is constant but I could be wrong.
+   if(!BRANDS.include?(brand))
+      BRANDS.push(brand);
+   end
   end
 
   def cobble
